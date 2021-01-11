28a1623ad7b9046a34417b5032e5fe14cdd7672a
Alexandros Ntousias <antousias@gmail.com>
2021-01-11 12:43:46 +0200

    Added integration tests for sessions

---
+++ tests/integration/sessions_test.go
@@ -0,0 +1,30 @@
+// +build integration
+
+package integration
+
+import (
+	"testing"
+)
+
+func TestSessions(t *testing.T) {
+	client := createClient()
+
+	sessions, err := client.Sessions().ListAll()
+	if err != nil {
+		t.Fatalf("Sessions.ListAll returned error: %v", err)
+	}
+	if sessions == nil {
+		t.Fatalf("Sessions.ListAll returned nil")
+	}
+
+	for _, session := range sessions {
+		sessionId := session.ID
+		session, err := client.Sessions().Read(sessionId)
+		if err != nil {
+			t.Fatalf("Sessions.Read returned error: %v", err)
+		}
+		if session == nil {
+			t.Fatalf("Sessions.Read returned nil")
+		}
+	}
+}
