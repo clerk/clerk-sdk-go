398d61ff13ef348c8c345ccf346efe537d4758f8
Alexandros Ntousias <antousias@gmail.com>
2021-02-03 14:45:27 +0200

    Modified verification integration tests to rely on env variables

--- tests/integration/clerk_test.go
+++ tests/integration/clerk_test.go
@@ -7,15 +7,27 @@ import (
 	"os"
 )
 
-func createClient() clerk.Client {
-	apiKey := os.Getenv("CLERK_API_KEY")
-	if apiKey == "" {
-		panic("Missing env variable CLERK_API_KEY")
-	}
+type key string
 
+const (
+	APIKey       = "CLERK_API_KEY"
+	SessionToken = "CLERK_SESSION_TOKEN"
+	SessionID    = "CLERK_SESSION_ID"
+)
+
+func createClient() clerk.Client {
+	apiKey := getEnv(APIKey)
 	client, err := clerk.NewClient(apiKey)
 	if err != nil {
 		panic("Unable to create Clerk client")
 	}
 	return client
+}
+
+func getEnv(k string) string {
+	envValue := os.Getenv(k)
+	if envValue == "" {
+		panic("Missing env variable " + k)
+	}
+	return envValue
 }
--- tests/integration/verification_test.go
+++ tests/integration/verification_test.go
@@ -27,7 +27,7 @@ func TestVerification_clientActiveSession(t *testing.T) {
 func TestVerification_verifySessionId(t *testing.T) {
 	client := createClient()
 
-	sessionId := "sess_1n8u0DZwnk3N4X9iDZ4eSxPfwQ0"
+	sessionId := getEnv(SessionID)
 	request := buildRequest(&sessionId)
 
 	session, err := client.Verification().Verify(request)
@@ -39,8 +39,6 @@ func TestVerification_verifySessionId(t *testing.T) {
 		t.Errorf("Was expecting session to be returned")
 	}
 }
-
-const dummySessionToken = "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJkZXYiOiJkdmJfMW44dHNvcWh6QkdBSTAwNWRVSXQ0YUNPbXhwIiwiaWQiOiJjbGllbnRfMW44dHpCZzZPMFV0bDIzQUxzSnNPOHd6UGltIiwicm90YXRpbmdfdG9rZW4iOiI5ZjJ2c2pocGhtb3J3enZ0ZXRhNGt5YjVjODViMDZtYzg0emI4ZHM1In0.03WXffBCv8MmRRqx_GzfKcpo8tPQ12gx8eZ_v7w2thZJmLPL-5N_-dFSDICpP2_0gOErnl9NKhFDjgC6TPpery0sLzNqSpxc12gb0VWJlYP78vI5daF8aqhUzefTY3buwFsSLJrdm1nxQViWBGc7HEJGsLvD5sYZxF1OYzi7eOy2k-k6ASFAg3CGJnjBmZy-jhUzgjntbflekfaSfBeYgWpWsuVXwieZ6ZdEeaypTQ20VZzAvhFX-TZocyf3p2aIS9yZRf5OL2n5p8Vy8yLoxhrkP9TWf7k6et7O3gKLaq4ym7cZL_iFOOiMOAIZcXWTfe37F6VA4qaNsUaVvd74Sg"
 
 func buildRequest(sessionId *string) *http.Request {
 	var request http.Request
@@ -56,7 +54,7 @@ func buildRequest(sessionId *string) *http.Request {
 	// add session token as cookie
 	sessionCookie := http.Cookie{
 		Name:  clerk.CookieSession,
-		Value: dummySessionToken,
+		Value: getEnv(SessionToken),
 	}
 	request.Header = make(map[string][]string)
 	request.AddCookie(&sessionCookie)
