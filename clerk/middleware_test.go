a43a59a0409b0c9f36c2449fffa8d7949d5de805
Haris Chaniotakis <chanioxaris@gmail.com>
2021-09-07 16:23:17 +0300

    feat: Change middleware to also handle AuthV2

--- clerk/middleware_test.go
+++ clerk/middleware_test.go
@@ -6,9 +6,14 @@ import (
 	"net/http"
 	"reflect"
 	"testing"
+	"time"
+
+	"gopkg.in/square/go-jose.v2/jwt"
+
+	"gopkg.in/square/go-jose.v2"
 )
 
-func TestMiddleware_addSessionToContext(t *testing.T) {
+func TestWithSession_addSessionToContext(t *testing.T) {
 	apiToken := "apiToken"
 	sessionId := "someSessionId"
 	sessionToken := "someSessionToken"
@@ -46,7 +51,7 @@ func TestMiddleware_addSessionToContext(t *testing.T) {
 	}
 }
 
-func TestMiddleware_returnsErrorIfVerificationFails(t *testing.T) {
+func TestWithSession_returnsErrorIfVerificationFails(t *testing.T) {
 	apiToken := "apiToken"
 	sessionId := "someSessionId"
 	sessionToken := "someSessionToken"
@@ -72,7 +77,138 @@ func TestMiddleware_returnsErrorIfVerificationFails(t *testing.T) {
 
 	resp, _ := client.Do(request, nil)
 
-	if resp.StatusCode != 400 {
+	if resp.StatusCode != http.StatusBadRequest {
+		t.Errorf("Was expecting 400 error code, found %v instead", resp.StatusCode)
+	}
+}
+
+func TestWithSession_addSessionClaimsToContext_Header(t *testing.T) {
+	c, mux, serverUrl, teardown := setup("apiToken")
+	defer teardown()
+
+	expectedClaims := dummySessionClaims
+
+	token, pubKey := testGenerateTokenJWT(t, expectedClaims, "kid")
+
+	client := c.(*client)
+	client.jwksCache.set(testBuildJWKS(t, pubKey, jose.RS256, "kid"))
+
+	dummyHandler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// this handler should be called after the middleware has added the `ActiveClaims`
+		claims := r.Context().Value(ActiveSessionClaims)
+		_ = json.NewEncoder(w).Encode(claims)
+	})
+
+	mux.Handle("/claims", WithSession(c)(dummyHandler))
+
+	req, _ := http.NewRequest(http.MethodGet, fmt.Sprintf("%s/claims", serverUrl), nil)
+	req.Header.Set("Authorization", token)
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	defer resp.Body.Close()
+
+	var got SessionClaims
+	_ = json.NewDecoder(resp.Body).Decode(&got)
+
+	if !reflect.DeepEqual(got, expectedClaims) {
+		t.Errorf("Response = %v, want %v", got, expectedClaims)
+	}
+}
+
+func TestWithSession_addSessionClaimsToContext_Cookie(t *testing.T) {
+	c, mux, serverUrl, teardown := setup("apiToken")
+	defer teardown()
+
+	expectedClaims := dummySessionClaims
+
+	token, pubKey := testGenerateTokenJWT(t, expectedClaims, "kid")
+
+	client := c.(*client)
+	client.jwksCache.set(testBuildJWKS(t, pubKey, jose.RS256, "kid"))
+
+	dummyHandler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// this handler should be called after the middleware has added the `ActiveClaims`
+		activeClaims := r.Context().Value(ActiveSessionClaims)
+		_ = json.NewEncoder(w).Encode(activeClaims)
+	})
+
+	mux.Handle("/claims", WithSession(c)(dummyHandler))
+
+	req, _ := http.NewRequest(http.MethodGet, fmt.Sprintf("%s/claims", serverUrl), nil)
+	req.AddCookie(&http.Cookie{
+		Name:     "__session",
+		Value:    token,
+		Secure:   true,
+		HttpOnly: true,
+	})
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	defer resp.Body.Close()
+
+	var got SessionClaims
+	_ = json.NewDecoder(resp.Body).Decode(&got)
+
+	if !reflect.DeepEqual(got, expectedClaims) {
+		t.Errorf("Response = %v, want %v", got, expectedClaims)
+	}
+}
+
+func TestWithSession_returnsErrorIfTokenVerificationFails(t *testing.T) {
+	c, mux, serverUrl, teardown := setup("apiToken")
+	defer teardown()
+
+	expectedClaims := dummySessionClaims
+	expectedClaims.Expiry = jwt.NewNumericDate(time.Now().Add(time.Second * -1))
+
+	token, pubKey := testGenerateTokenJWT(t, expectedClaims, "kid")
+
+	client := c.(*client)
+	client.jwksCache.set(testBuildJWKS(t, pubKey, jose.RS256, "kid"))
+
+	dummyHandler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// this handler should be called after the middleware has added the `ActiveClaims`
+		activeClaims := r.Context().Value(ActiveSessionClaims)
+		_ = json.NewEncoder(w).Encode(activeClaims)
+	})
+
+	mux.Handle("/claims", WithSession(c)(dummyHandler))
+
+	req, _ := http.NewRequest(http.MethodGet, fmt.Sprintf("%s/claims", serverUrl), nil)
+	req.Header.Set("Authorization", token)
+
+	resp, _ := http.DefaultClient.Do(req)
+
+	if resp.StatusCode != http.StatusUnauthorized {
+		t.Errorf("Was expecting 401 error code, found %v instead", resp.StatusCode)
+	}
+}
+
+func TestWithSession_returnsErrorIfTokenMissing(t *testing.T) {
+	apiToken := "apiToken"
+
+	c, mux, serverUrl, teardown := setup(apiToken)
+	defer teardown()
+
+	dummyHandler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// this handler should be called after the middleware has added the `ActiveSession`
+		t.Errorf("This should never be called!")
+	})
+
+	mux.Handle("/claims", WithSession(c)(dummyHandler))
+
+	req, _ := http.NewRequest(http.MethodGet, fmt.Sprintf("%s/claims", serverUrl), nil)
+
+	resp, _ := http.DefaultClient.Do(req)
+
+	if resp.StatusCode != http.StatusBadRequest {
 		t.Errorf("Was expecting 400 error code, found %v instead", resp.StatusCode)
 	}
 }
