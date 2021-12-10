dc87caf1d112319eaa33ba43d402f444aaade0de
Haris Chaniotakis <chanioxaris@gmail.com>
2021-12-10 12:49:04 +0200

    fix: Take Authorization header into account if included in the request

--- clerk/middleware_v2_test.go
+++ clerk/middleware_v2_test.go
@@ -28,3 +28,26 @@ func TestWithSessionV2_nonBrowserRequest(t *testing.T) {
 		t.Fatal(err)
 	}
 }
+
+func TestWithSessionV2_emptyAuthorizationHeader(t *testing.T) {
+	c, mux, serverUrl, teardown := setup("test_dummy")
+	defer teardown()
+
+	dummyHandler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// Should be signed out
+		_, ok := r.Context().Value(ActiveSessionClaims).(*SessionClaims)
+		if ok {
+			t.Error("Expected session claims not to be present in request context")
+		}
+	})
+
+	mux.Handle("/dummy", WithSessionV2(c)(dummyHandler))
+
+	req, _ := http.NewRequest(http.MethodGet, fmt.Sprintf("%s/dummy", serverUrl), nil)
+	req.Header.Set("Authorization", "")
+
+	_, err := http.DefaultClient.Do(req)
+	if err != nil {
+		t.Fatal(err)
+	}
+}
