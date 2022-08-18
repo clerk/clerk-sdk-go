5f054dc20e542729130635c8db317276fb55e5e2
Alexandros Ntousias <antousias@gmail.com>
2022-08-18 12:48:01 +0300

    feat: Added blocklists in Go SDK
    This commit adds functions to handle blocklists.
    In particular, it includes the following:
    * `CreateIdentifier`: Creates a new blocklist identifier. This
       identifier can be an email address or domain, a phone number or
       a web3 wallet.
    * `ListAllIdentifiers`: Returns all blocklist identifiers
    * `DeleteIdentifier`: Deletes the blocklist identifier that
      corresponds to the given id

---
+++ clerk/blocklists.go
@@ -0,0 +1,57 @@
+package clerk
+
+import (
+	"net/http"
+)
+
+type BlocklistsService service
+
+type BlocklistIdentifierResponse struct {
+	Object     string `json:"object"`
+	ID         string `json:"id"`
+	Identifier string `json:"identifier"`
+	CreatedAt  int64  `json:"created_at"`
+	UpdatedAt  int64  `json:"updated_at"`
+}
+
+type CreateBlocklistIdentifierParams struct {
+	Identifier string `json:"identifier"`
+}
+
+func (s *BlocklistsService) CreateIdentifier(params CreateBlocklistIdentifierParams) (*BlocklistIdentifierResponse, error) {
+	req, _ := s.client.NewRequest(http.MethodPost, BlocklistsUrl, &params)
+
+	var blocklistIdentifierResponse BlocklistIdentifierResponse
+	_, err := s.client.Do(req, &blocklistIdentifierResponse)
+	if err != nil {
+		return nil, err
+	}
+	return &blocklistIdentifierResponse, nil
+}
+
+func (s *BlocklistsService) DeleteIdentifier(identifierID string) (*DeleteResponse, error) {
+	req, _ := s.client.NewRequest(http.MethodDelete, BlocklistsUrl+"/"+identifierID)
+
+	var deleteResponse DeleteResponse
+	_, err := s.client.Do(req, &deleteResponse)
+	if err != nil {
+		return nil, err
+	}
+	return &deleteResponse, nil
+}
+
+type BlocklistIdentifiersResponse struct {
+	Data       []*BlocklistIdentifierResponse `json:"data"`
+	TotalCount int64                          `json:"total_count"`
+}
+
+func (s *BlocklistsService) ListAllIdentifiers() (*BlocklistIdentifiersResponse, error) {
+	req, _ := s.client.NewRequest(http.MethodGet, BlocklistsUrl)
+
+	var blocklistIdentifiersResponse *BlocklistIdentifiersResponse
+	_, err := s.client.Do(req, &blocklistIdentifiersResponse)
+	if err != nil {
+		return nil, err
+	}
+	return blocklistIdentifiersResponse, nil
+}
