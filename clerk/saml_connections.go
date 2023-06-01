02f1e2990e184c38b4205245a9e929334cd98a49
Haris Chaniotakis <haris@clerk.dev>
2023-06-01 18:52:06 +0300

    feat: Introduce new sync_user_attributes SAML Connection field and ability to update it

--- clerk/saml_connections.go
+++ clerk/saml_connections.go
@@ -8,20 +8,21 @@ import (
 type SAMLConnectionsService service
 
 type SAMLConnection struct {
-	ID             string `json:"id"`
-	Object         string `json:"object"`
-	Name           string `json:"name"`
-	Domain         string `json:"domain"`
-	IdpEntityID    string `json:"idp_entity_id"`
-	IdpSsoURL      string `json:"idp_sso_url"`
-	IdpCertificate string `json:"idp_certificate"`
-	AcsURL         string `json:"acs_url"`
-	SPEntityID     string `json:"sp_entity_id"`
-	Active         bool   `json:"active"`
-	Provider       string `json:"provider"`
-	UserCount      int64  `json:"user_count"`
-	CreatedAt      int64  `json:"created_at"`
-	UpdatedAt      int64  `json:"updated_at"`
+	ID                 string `json:"id"`
+	Object             string `json:"object"`
+	Name               string `json:"name"`
+	Domain             string `json:"domain"`
+	IdpEntityID        string `json:"idp_entity_id"`
+	IdpSsoURL          string `json:"idp_sso_url"`
+	IdpCertificate     string `json:"idp_certificate"`
+	AcsURL             string `json:"acs_url"`
+	SPEntityID         string `json:"sp_entity_id"`
+	Active             bool   `json:"active"`
+	Provider           string `json:"provider"`
+	UserCount          int64  `json:"user_count"`
+	SyncUserAttributes bool   `json:"sync_user_attributes"`
+	CreatedAt          int64  `json:"created_at"`
+	UpdatedAt          int64  `json:"updated_at"`
 }
 
 type ListSAMLConnectionsResponse struct {
@@ -99,12 +100,13 @@ func (s SAMLConnectionsService) Create(params *CreateSAMLConnectionParams) (*SAM
 }
 
 type UpdateSAMLConnectionParams struct {
-	Name           *string `json:"name,omitempty"`
-	Domain         *string `json:"domain,omitempty"`
-	IdpEntityID    *string `json:"idp_entity_id,omitempty"`
-	IdpSsoURL      *string `json:"idp_sso_url,omitempty"`
-	IdpCertificate *string `json:"idp_certificate,omitempty"`
-	Active         *bool   `json:"active,omitempty"`
+	Name               *string `json:"name,omitempty"`
+	Domain             *string `json:"domain,omitempty"`
+	IdpEntityID        *string `json:"idp_entity_id,omitempty"`
+	IdpSsoURL          *string `json:"idp_sso_url,omitempty"`
+	IdpCertificate     *string `json:"idp_certificate,omitempty"`
+	Active             *bool   `json:"active,omitempty"`
+	SyncUserAttributes *bool   `json:"sync_user_attributes,omitempty"`
 }
 
 func (s SAMLConnectionsService) Update(id string, params *UpdateSAMLConnectionParams) (*SAMLConnection, error) {
--- clerk/saml_connections_test.go
+++ clerk/saml_connections_test.go
@@ -112,6 +112,7 @@ func TestSAMLConnectionsService_Create(t *testing.T) {
 func TestSAMLConnectionsService_Update(t *testing.T) {
 	expectedName := "New name for Testing SAML"
 	expectedActive := true
+	expectedSyncUserAttributes := false
 	dummyResponse := dummySAMLConnectionUpdatedJSON
 
 	c, mux, _, teardown := setup("token")
@@ -126,8 +127,9 @@ func TestSAMLConnectionsService_Update(t *testing.T) {
 	})
 
 	updateParams := &UpdateSAMLConnectionParams{
-		Name:   &expectedName,
-		Active: &expectedActive,
+		Name:               &expectedName,
+		Active:             &expectedActive,
+		SyncUserAttributes: &expectedSyncUserAttributes,
 	}
 
 	got, err := c.SAMLConnections().Update(dummySAMLConnectionID, updateParams)
@@ -184,7 +186,8 @@ const (
 	"sp_entity_id": "` + "https://clerk.example.com/acs" + dummySAMLConnectionID + `",
 	"active": false,
 	"provider": "saml_custom",
-	"user_count": 3
+	"user_count": 3,
+	"sync_user_attributes": true
 }`
 
 	dummySAMLConnectionUpdatedJSON = `
@@ -198,7 +201,10 @@ const (
 	"idp_certificate": "` + dummySAMLConnectionCertificate + `",
 	"acs_url": "` + "https://clerk.example.com/v1/saml/acs" + dummySAMLConnectionID + `",
 	"sp_entity_id": "` + "https://clerk.example.com/acs" + dummySAMLConnectionID + `",
-	"active": true
+	"active": true,
+	"provider": "saml_custom",
+	"user_count": 3,
+	"sync_user_attributes": false
 }`
 
 	dummySAMLConnectionCertificate = `MIIDBzCCAe+gAwIBAgIJAPr/Mrlc8EGhMA0GCSqGSIb3DQEBBQUAMBoxGDAWBgNVBAMMD3d3dy5leGFtcGxlLmNvbTAeFw0xNTEyMjgxOTE5NDVaFw0yNTEyMjUxOTE5NDVaMBoxGDAWBgNVBAMMD3d3dy5leGFtcGxlLmNvbTCCASIwDQYJKoZIhvcNAQEBBQADggEPADCCAQoCggEBANDoWzLos4LWxTn8Gyu2lEbl4WcelUbgLN5zYm4ron8Ahs+rvcsu2zkdD/s6jdGJI8WqJKhYK2u61ygnXgAZqC6ggtFPnBpizcDzjgND2g+aucSoUODHt67f0fQuAmupN/zp5MZysJ6IHLJnYLNpfJYk96lRz9ODnO1Mpqtr9PWxm+pz7nzq5F0vRepkgpcRxv6ufQBjlrFytccyEVdXrvFtkjXcnhVVNSR4kHuOOMS6D7pebSJ1mrCmshbD5SX1jXPBKFPAjozYX6PxqLxUx1Y4faFEf4MBBVcInyB4oURNB2s59hEEi2jq9izNE7EbEK6BY5sEhoCPl9m32zE6ljkCAwEAAaNQME4wHQYDVR0OBBYEFB9ZklC1Ork2zl56zg08ei7ss/+iMB8GA1UdIwQYMBaAFB9ZklC1Ork2zl56zg08ei7ss/+iMAwGA1UdEwQFMAMBAf8wDQYJKoZIhvcNAQEFBQADggEBAAVoTSQ5pAirw8OR9FZ1bRSuTDhY9uxzl/OL7lUmsv2cMNeCB3BRZqm3mFt+cwN8GsH6f3uvNONIhgFpTGN5LEcXQz89zJEzB+qaHqmbFpHQl/sx2B8ezNgT/882H2IH00dXESEfy/+1gHg2pxjGnhRBN6el/gSaDiySIMKbilDrffuvxiCfbpPN0NRRiPJhd2ay9KuL/RxQRl1gl9cHaWiouWWba1bSBb2ZPhv2rPMUsFo98ntkGCObDX6Y1SpkqmoTbrsbGFsTG2DLxnvr4GdN1BSr0Uu/KV3adj47WkXVPeMYQti/bQmxQB8tRFhrw80qakTLUzreO96WzlBBMtY=`
