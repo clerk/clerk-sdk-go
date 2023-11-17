278141043c8f6adcfb2ec0a7e50a5204a5034410
Haris Chaniotakis <haris@clerk.dev>
2023-11-17 14:10:49 +0200

    feat: Denote if organization role can be used as the creator
    We expose a new property as part of the role response, which denotes if the
    role can be used as the default organization creator role.
    We also align the dummy Role JSON objects with real response objects

--- clerk/roles.go
+++ clerk/roles.go
@@ -1,14 +1,15 @@
 package clerk
 
 type Role struct {
-	Object      string       `json:"object"`
-	ID          string       `json:"id"`
-	Name        string       `json:"name"`
-	Key         string       `json:"key"`
-	Description string       `json:"description"`
-	Permissions []Permission `json:"permissions"`
-	CreatedAt   int64        `json:"created_at"`
-	UpdatedAt   int64        `json:"updated_at"`
+	Object            string       `json:"object"`
+	ID                string       `json:"id"`
+	Name              string       `json:"name"`
+	Key               string       `json:"key"`
+	Description       string       `json:"description"`
+	Permissions       []Permission `json:"permissions"`
+	IsCreatorEligible bool         `json:"is_creator_eligible"`
+	CreatedAt         int64        `json:"created_at"`
+	UpdatedAt         int64        `json:"updated_at"`
 }
 
 type RolesResponse struct {
