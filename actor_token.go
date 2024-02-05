60ff523d030c4c12b66cb9b01317b29998811f53
Giannis Katsanos <giannis@clerk.dev>
2024-02-05 17:45:54 +0200

    fix: Include actor token URL (#215)
    Added the ActorToken.URL field.

--- actor_token.go
+++ actor_token.go
@@ -9,6 +9,7 @@ type ActorToken struct {
 	UserID    string          `json:"user_id"`
 	Actor     json.RawMessage `json:"actor"`
 	Token     string          `json:"token,omitempty"`
+	URL       *string         `json:"url,omitempty"`
 	Status    string          `json:"status"`
 	CreatedAt int64           `json:"created_at"`
 	UpdatedAt int64           `json:"updated_at"`
