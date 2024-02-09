8e9959abefbfc8553de1a87cdb4f2ba758085638
Giannis Katsanos <giannis@clerk.dev>
2024-02-09 10:48:24 +0200

    feat: Svix Webhooks API
    Added support for the Svix Webhooks API with Create, Delete and
    RefreshURL operations.

---
+++ svixwebhook/client.go
@@ -0,0 +1,52 @@
+// Package svixwebhook provides the Svix Webhooks API.
+package svixwebhook
+
+import (
+	"context"
+	"net/http"
+
+	"github.com/clerk/clerk-sdk-go/v2"
+)
+
+//go:generate go run ../cmd/gen/main.go
+
+const path = "/webhooks/svix"
+
+// Client is used to invoke the Organizations API.
+type Client struct {
+	Backend clerk.Backend
+}
+
+type ClientConfig struct {
+	clerk.BackendConfig
+}
+
+func NewClient(config *ClientConfig) *Client {
+	return &Client{
+		Backend: clerk.NewBackend(&config.BackendConfig),
+	}
+}
+
+// Create creates a Svix app.
+func (c *Client) Create(ctx context.Context) (*clerk.SvixWebhook, error) {
+	req := clerk.NewAPIRequest(http.MethodPost, path)
+	resource := &clerk.SvixWebhook{}
+	err := c.Backend.Call(ctx, req, resource)
+	return resource, err
+}
+
+// Delete deletes the Svix app.
+func (c *Client) Delete(ctx context.Context) (*clerk.SvixWebhook, error) {
+	req := clerk.NewAPIRequest(http.MethodDelete, path)
+	resource := &clerk.SvixWebhook{}
+	err := c.Backend.Call(ctx, req, resource)
+	return resource, err
+}
+
+// RefreshURL generates a new URL for accessing Svix's dashboard.
+func (c *Client) RefreshURL(ctx context.Context) (*clerk.SvixWebhook, error) {
+	req := clerk.NewAPIRequest(http.MethodPost, "/webhooks/svix_url")
+	resource := &clerk.SvixWebhook{}
+	err := c.Backend.Call(ctx, req, resource)
+	return resource, err
+}
