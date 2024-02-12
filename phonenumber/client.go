41143209f761367dc3e7527a4866736261122a5f
Giannis Katsanos <giannis@clerk.dev>
2024-02-12 15:42:47 +0200

    feat: Phone numbers API
    Added support for the Phone Numbers API. Available operations are
    Create, Get, Update and Delete.

---
+++ phonenumber/client.go
@@ -0,0 +1,88 @@
+// Package phonenumber provides the Phone Numbers API.
+package phonenumber
+
+import (
+	"context"
+	"net/http"
+
+	"github.com/clerk/clerk-sdk-go/v2"
+)
+
+//go:generate go run ../cmd/gen/main.go
+
+const path = "/phone_numbers"
+
+// Client is used to invoke the Phone Numbers API.
+type Client struct {
+	Backend clerk.Backend
+}
+
+type ClientConfig struct {
+	clerk.BackendConfig
+}
+
+func NewClient(config *ClientConfig) *Client {
+	return &Client{
+		Backend: clerk.NewBackend(&config.BackendConfig),
+	}
+}
+
+type CreateParams struct {
+	clerk.APIParams
+	UserID      *string `json:"user_id,omitempty"`
+	PhoneNumber *string `json:"phone_number,omitempty"`
+	Verified    *bool   `json:"verified,omitempty"`
+	Primary     *bool   `json:"primary,omitempty"`
+}
+
+// Create creates a new phone number.
+func (c *Client) Create(ctx context.Context, params *CreateParams) (*clerk.PhoneNumber, error) {
+	req := clerk.NewAPIRequest(http.MethodPost, path)
+	req.SetParams(params)
+	resource := &clerk.PhoneNumber{}
+	err := c.Backend.Call(ctx, req, resource)
+	return resource, err
+}
+
+// Get retrieves a phone number.
+func (c *Client) Get(ctx context.Context, id string) (*clerk.PhoneNumber, error) {
+	path, err := clerk.JoinPath(path, id)
+	if err != nil {
+		return nil, err
+	}
+	req := clerk.NewAPIRequest(http.MethodGet, path)
+	resource := &clerk.PhoneNumber{}
+	err = c.Backend.Call(ctx, req, resource)
+	return resource, err
+}
+
+type UpdateParams struct {
+	clerk.APIParams
+	Verified *bool `json:"verified,omitempty"`
+	Primary  *bool `json:"primary,omitempty"`
+}
+
+// Update updates the phone number specified by id.
+func (c *Client) Update(ctx context.Context, id string, params *UpdateParams) (*clerk.PhoneNumber, error) {
+	path, err := clerk.JoinPath(path, id)
+	if err != nil {
+		return nil, err
+	}
+	req := clerk.NewAPIRequest(http.MethodPatch, path)
+	req.SetParams(params)
+	resource := &clerk.PhoneNumber{}
+	err = c.Backend.Call(ctx, req, resource)
+	return resource, err
+}
+
+// Delete deletes a phone number.
+func (c *Client) Delete(ctx context.Context, id string) (*clerk.DeletedResource, error) {
+	path, err := clerk.JoinPath(path, id)
+	if err != nil {
+		return nil, err
+	}
+	req := clerk.NewAPIRequest(http.MethodDelete, path)
+	resource := &clerk.DeletedResource{}
+	err = c.Backend.Call(ctx, req, resource)
+	return resource, err
+}
