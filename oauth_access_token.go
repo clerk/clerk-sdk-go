5151b40c0a29b3b3210811debefc3bedd97d72f9
Konstantinos Pittas <konstantinos.pittas+github@gmail.com>
2024-03-12 13:24:25 +0200

    feat: Add `external_account_id` to OAuth access token response (#275)

--- oauth_access_token.go
+++ oauth_access_token.go
@@ -3,11 +3,12 @@ package clerk
 import "encoding/json"
 
 type OAuthAccessToken struct {
-	Object         string          `json:"object"`
-	Token          string          `json:"token"`
-	Provider       string          `json:"provider"`
-	PublicMetadata json.RawMessage `json:"public_metadata"`
-	Label          *string         `json:"label"`
+	ExternalAccountID string          `json:"external_account_id"`
+	Object            string          `json:"object"`
+	Token             string          `json:"token"`
+	Provider          string          `json:"provider"`
+	PublicMetadata    json.RawMessage `json:"public_metadata"`
+	Label             *string         `json:"label"`
 	// Only set in OAuth 2.0 tokens
 	Scopes []string `json:"scopes,omitempty"`
 	// Only set in OAuth 1.0 tokens
