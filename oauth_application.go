7107ca9bd97a4c1d988c1a7d8aceef6694206d2f
Jacob Foshee <jacob.foshee@clerk.dev>
2025-07-21 17:15:20 -0500

    feat(oauth): add `OAuthApplication.ClientImageURL`

--- oauth_application.go
+++ oauth_application.go
@@ -8,6 +8,7 @@ type OAuthApplication struct {
 	Name                  string   `json:"name"`
 	ClientID              string   `json:"client_id"`
 	ClientSecret          *string  `json:"client_secret,omitempty"`
+	ClientImageURL        *string  `json:"client_image_url"`
 	Public                bool     `json:"public"`
 	DynamicallyRegistered bool     `json:"dynamically_registered"`
 	ConsentScreenEnabled  bool     `json:"consent_screen_enabled"`
