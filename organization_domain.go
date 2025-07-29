16e173a7a9431879d55a7f51cdb784cf54d0daee
Nicolas Lopes <nicolas@clerk.dev>
2025-07-29 18:00:58 -0300

    add public organization data to organization domains

--- organization_domain.go
+++ organization_domain.go
@@ -20,6 +20,7 @@ type OrganizationDomain struct {
 	TotalPendingSuggestions int64                           `json:"total_pending_suggestions"`
 	CreatedAt               int64                           `json:"created_at"`
 	UpdatedAt               int64                           `json:"updated_at"`
+	PublicOrganizationData  *PublicOrganizationData         `json:"public_organization_data,omitempty"`
 }
 
 type OrganizationDomainList struct {
