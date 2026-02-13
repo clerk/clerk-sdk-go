d2afa72dd5acde547d1f5a46410881f701cf13c1
Nicolas Lopes <nicolas@clerk.dev>
2026-02-13 20:10:07 -0300

    lint

--- scim_directory.go
+++ scim_directory.go
@@ -3,16 +3,16 @@ package clerk
 // SCIMDirectory represents a SCIM directory resource.
 type SCIMDirectory struct {
 	APIResource
-	Object                 string  `json:"object"`
-	ID                     string  `json:"id"`
-	Name                   string  `json:"name"`
-	EnterpriseConnectionID *string `json:"enterprise_connection_id"`
-	EndpointURL            string  `json:"endpoint_url"`
-	Provider               string  `json:"provider"`
-	Enabled                bool    `json:"enabled"`
-	APIKey                 *string `json:"api_key,omitempty"`
-	CreatedAt              int64   `json:"created_at"`
-	UpdatedAt              int64   `json:"updated_at"`
+	Object                 string            `json:"object"`
+	ID                     string            `json:"id"`
+	Name                   string            `json:"name"`
+	EnterpriseConnectionID *string           `json:"enterprise_connection_id"`
+	EndpointURL            string            `json:"endpoint_url"`
+	Provider               string            `json:"provider"`
+	Enabled                bool              `json:"enabled"`
+	APIKey                 *string           `json:"api_key,omitempty"`
+	CreatedAt              int64             `json:"created_at"`
+	UpdatedAt              int64             `json:"updated_at"`
 	AttributeMapping       map[string]string `json:"attribute_mapping,omitempty"`
 }
 
