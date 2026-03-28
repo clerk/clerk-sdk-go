4e788062249f1a3b92e71fde363b648c4fe9c573
Laura Beatris <48022589+LauraBeatris@users.noreply.github.com>
2026-03-28 18:05:20 -0300

    Add `Authenticatable` to enterprise connections

--- samlconnection/client.go
+++ samlconnection/client.go
@@ -118,6 +118,7 @@ type UpdateParams struct {
 	AllowOrganizationAccountLinking  *bool                   `json:"allow_organization_account_linking,omitempty"`
 	ForceAuthn                       *bool                   `json:"force_authn,omitempty"`
 	ConsentVerifiedDomainsDeletion   *bool                   `json:"consent_verified_domains_deletion,omitempty"`
+	Authenticatable                  *bool                   `json:"authenticatable,omitempty"`
 	// CustomAttributes is an Experimental feature, not available for all customers.
 	CustomAttributes *[]clerk.CustomAttribute `json:"custom_attributes,omitempty"`
 }
