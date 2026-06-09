64c8528573f6780631b9e1c9794bd05e3b01a1fd
Nicolas Lopes <nicolas@clerk.dev>
2026-06-09 10:10:13 -0300

    feat: add disable_jit to enterprise + saml connection update params and responses

--- enterprise_connection.go
+++ enterprise_connection.go
@@ -20,6 +20,7 @@ type EnterpriseConnection struct {
 	ForceAuthn                       bool     `json:"force_authn"`
 	AllowOrganizationAccountLinking  bool     `json:"allow_organization_account_linking"`
 	Authenticatable                  bool     `json:"authenticatable"`
+	DisableJIT                       bool     `json:"disable_jit"`
 	CreatedAt                        int64    `json:"created_at"`
 	UpdatedAt                        int64    `json:"updated_at"`
 
